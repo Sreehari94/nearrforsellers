91bada89f1b29dfb7189ddaaffab44fb69fa33dc
Hari Krishnan <lookworld4@gmail.com>
2017-12-03 16:18:30 +0530

    how it work section

--- js/ga.js
+++ js/ga.js
@@ -1,6 +1,12 @@
-var showServicesSection=function() {
+var showWhyNearrSection=function() {
     ga("send", {
-        hitType: "event", eventCategory: "ServicesSection", eventAction: "User clicked on services in menu bar", eventLabel: "USER_CLICKED_ON_SERVICES_IN_MENU_BAR", eventValue: 0
+        hitType: "event", eventCategory: "WhyNearrSection", eventAction: "User clicked on why nearr in menu bar", eventLabel: "USER_CLICKED_ON_WHY_NEARR_IN_MENU_BAR", eventValue: 0
+    }
+    )
+},
+showHowItWorkSection=function() {
+    ga("send", {
+        hitType: "event", eventCategory: "HowItWorkSection", eventAction: "User clicked on how it work in menu bar", eventLabel: "USER_CLICKED_ON_HOW_IT_WORK_IN_MENU_BAR", eventValue: 0
     }
     )
 },
@@ -10,9 +16,9 @@ showContactSection=function() {
     }
     )
 },
-showSignUpForm=function() {
+showRequestCallbackForm=function() {
     ga("send", {
-        hitType: "event", eventCategory: "SellerSignUpForm", eventAction: "User clicked on seller signup form button", eventLabel: "USER_CLICKED_ON_SELLER_SIGNUP_BUTTON", eventValue: 0
+        hitType: "event", eventCategory: "SellerRequestCallBackForm", eventAction: "User clicked on request callback form button", eventLabel: "USER_CLICKED_ON_REQUEST_CALLBACK_LINK", eventValue: 0
     }
     )
 };
